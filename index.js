be18c05980da5e932387973df028a598743c675b
bingyangliu <bingyangliu@bytedance.com>
2025-04-02 10:11:05 +0800

    feat: 添加窗口边缘碰撞检测、留白

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const rootElement = document.documentElement
 const key = 'data-testid'
 const tooltipPositionStore = []
 
+// 其他：
+// 和窗口边缘碰撞时候，会流出来10px的留白
+const windowWhiteSpace = 10
+
+
 const hasDataTestId = (node) => {
     if (node instanceof HTMLElement) {
         return node.hasAttribute(key)
@@ -14,15 +19,23 @@ const hasDataTestId = (node) => {
 // 这里需要检查是否:
 // 1. 和其他date-TestId碰撞
 // 2. 和边缘碰撞
+// 规则：
+// 1. 和tooltip碰撞的话，高度升高
+// 2. 和左右边缘碰撞的话，向相反的方向移动
 const getTopAndLeftImpl = (targetElement, tooltip) => {
     const dataTestId = targetElement.getAttribute(key)
     const rect = targetElement.getBoundingClientRect()
 
+    // tooltip的位置信息
     let top = rect.top + window.scrollY
-    const left = rect.right + window.scrollX
+    let left = rect.right + window.scrollX
     const width = tooltip.offsetWidth
     const height = tooltip.offsetHeight
 
+    const windowLeft = windowWhiteSpace
+    const windowRight = window.innerWidth - windowWhiteSpace
+
+    // 检测是否和已有的tooltip碰撞
     for (let i = 0; i < tooltipPositionStore.length; i++) {
         const existingPosition = tooltipPositionStore[i];
         const existingTop = existingPosition.top;
@@ -31,14 +44,6 @@ const getTopAndLeftImpl = (targetElement, tooltip) => {
         const existingHeight = existingPosition.height;
 
         const isCollide = () => {
-            console.log('self position', {
-                dataTestId,
-                top,
-                left,
-                width,
-                height
-            })
-            console.log('self existingPosition', existingPosition)
             return !(top + height <= existingTop ||
                 existingTop + existingHeight <= top ||
                 left + width <= existingLeft ||
@@ -51,6 +56,20 @@ const getTopAndLeftImpl = (targetElement, tooltip) => {
             top -= 10;
             // 这里减去1是为了重新检查碰撞
             i -= 1;
+        }
+    }
+
+    //检测是否和左右边缘碰撞
+    while (true) {
+        // TODO: 此处不考虑左右滑动的界面,后续添加
+        if (windowRight - windowLeft < width) break;
+
+        if (left >= windowLeft && left + width <= windowRight)break;
+
+        if (left < windowLeft) {
+            left += 10
+        } else if (left + width > windowLeft){
+            left -= 10
         }
     }
 
